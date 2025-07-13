442c32f12e1098f3c037e95e2050d2ec05147bca
kaushal <kolhekaushal2003@gmail.com>
2025-07-13 23:41:01 +0530

    Fix login redirect issue by setting LOGIN_URL

--- collab_editor/settings.py
+++ collab_editor/settings.py
@@ -133,6 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#custom telling django to go t specific location.
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/documents/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 ASGI_APPLICATION = 'collab_editor.asgi.application'
